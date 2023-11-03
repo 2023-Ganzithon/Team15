1f1b2698242bd55904621a08e3f495b8394293cb
yushinc <cys74351@gmail.com>
2023-11-03 17:37:39 +0900

    feat: Post, Comment, Video 모델 생성

--- post/models.py
+++ post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# 게시글
+class Post(models.Model):
+    userId = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    regTime = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=False)
+
+    like = models.ManyToManyField('user.CustomUser', related_name="like_posts")
+    buy = models.ManyToManyField('user.CustomUser', related_name="buy_posts")
+    bookmark = models.ManyToManyField('user.CustomUser', related_name="bookmark_posts")
+
+# 댓글
+class Comment(models.Model):
+    userId = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
+    postId = models.ForeignKey('post.Post', on_delete=models.CASCADE)
+    content = models.TextField()
+    anonymous = models.BooleanField(default=True) # True = 익명 / False = 실명
+
+
+# 동영상
+class Video(models.Model):
+    postId = models.ForeignKey('post.Post', on_delete=models.CASCADE)
+    videoKey = models.CharField()
