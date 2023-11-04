47d9b017fdd938cbac020c6b09a210354ae4867c
ty2622 <ty2622@likelion.org>
2023-11-04 11:28:05 +0900

    feat : 챌린지 참여/주제 모델 생성

--- challenge/models.py
+++ challenge/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class OngoingChallenge(models.Model):
+    check = models.BooleanField(default=False)# True = 성공/False = 실패
+    cDate = models.DateTimeField(auto_now_add=True)
+    cImage = models.ImageField(null=False)
+    cId = models.ForeignKey('challenge.challenge', on_delete=models.CASCADE)
+    userId = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
+
+
+class challenge(models.Model):
+    cName = models.CharField(max_length=100)
