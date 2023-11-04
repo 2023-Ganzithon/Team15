7110214c56f5931a424d7e422c8f2744e1d70f34
ty2622 <ty2622@likelion.org>
2023-11-04 20:39:59 +0900

    feat : 로그인, 로그아웃 기능 구현

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import signup_view
+from .views import signup_view,login_view,logout_view,test_view
 urlpatterns = [
-    path('signup/', signup_view, name='signup')
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('test/',test_view, name='test'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model,login,logout
 from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
 User = get_user_model()
 
 def signup_view(request):
@@ -15,13 +16,26 @@ def signup_view(request):
                 username=request.POST['username'],
                 password=request.POST['password1'],
             )
-            
-            return redirect('/')#메인페이지로 이동(수정필요)
-        return render(request, 'signup.html')#회원가입 실패시
+                
+            return redirect('test')#메인페이지로 이동(수정필요)
+        return render(request, 'signup.html',{'error': '회원가입 실패'})#회원가입 실패시
         
 
 def login_view(request):
     if request.method == 'GET':#로그인 HTML응답
-        pass
-    else:
-        pass
+        return render(request, 'login.html')
+    else:#POST
+        form =AuthenticationForm(request, request.POST)
+        if form.is_valid():#로그인 인증 성공
+            login(request, form.user_cache)
+            return redirect('test')#메인 페이지로 이동(수정필요)
+        else:#로그인 실패
+           return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+        
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('test')#메인페이지로 이동(수정필요)
+
+def test_view(request):
+    return render(request, 'test.html')
