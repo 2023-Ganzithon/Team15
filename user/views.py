bb67c71ed297751964191473a017808aef91626e
ty2622 <ty2622@likelion.org>
2023-11-04 17:31:40 +0900

    feat : 회원가입 기능 구현 및 html구현

--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+User = get_user_model()
 
-# Create your views here.
+def signup_view(request):
+    #GET 요청시 HTML 응답
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    
+    else:
+        #POST 요청시 데이터 확인 후 회원 생성
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+            )
+            
+            return redirect('/')#메인페이지로 이동(수정필요)
+        return render(request, 'signup.html')#회원가입 실패시
+        
+
+def login_view(request):
+    if request.method == 'GET':#로그인 HTML응답
+        pass
+    else:
+        pass
