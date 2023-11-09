d18774967bbdd88bf5dff304a534e3e6d495f1fc
ty2622 <ty2622@likelion.org>
2023-11-09 17:29:55 +0900

    fix 챌린지부분

--- challenge/views.py
+++ challenge/views.py
@@ -5,21 +5,23 @@ from .models import challenge, OngoingChallenge
 from user.models import CustomUser
 #챌린지 홈 + 주제 선택
 first = 1
-cname = 0
+
 def challenge_home(request):
-    global first,cname
+    global first
     user=request.user
     clist=challenge.objects.all()
-    context={"cId": cname}
+    
     if first == 1:
         cname= random.choice(clist)
         context={"cId": cname}
         first=0
         OngoingChallenge.objects.create(
             userId=request.user,
-            cId = cname
-        )
-
+            cId = cname,
+        )   
+    update= OngoingChallenge.objects.last()
+    cname=update.cId
+    context={"cId": cname}
 
     if request.method == 'GET':#html응답
         return render(request, 'challenge_home.html', context)
@@ -30,6 +32,9 @@ def challenge_home(request):
             cSelect.cSelect -=1
             cSelect.save()
             context={"cId":cname}
+            update= OngoingChallenge.objects.last()
+            update.cId = cname
+            update.save()
         return render(request, 'challenge_home.html', context)
 
 
@@ -37,7 +42,8 @@ def challenge_home(request):
 def challenge_upload(request):
     user = request.user
     global cname
-    context={"cId":cname}
+    upload= OngoingChallenge.objects.last()
+    context={"cId":upload.cId}
     
     # 생성 폼
     if request.method == 'GET':
@@ -45,10 +51,11 @@ def challenge_upload(request):
 
     if request.method == 'POST':
         #아마 주제도 포함
-        image = request.FILES.get('image')
-        upload= OngoingChallenge.objects.last()
+        image = request.FILES.get('cImage')
+        
         upload.cDate=timezone.now()
         upload.cImage=image
+         
         upload.save()
 
         return redirect('challenge:challenge_detail')
